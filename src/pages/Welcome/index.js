f7d15fb0f5a3580b622125fab424123b66e5f5ec
Ivan da Silva Rosa <iivan.si@msn.com>
2022-04-05 00:36:58 -0300

    welcome page, more one animation

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -7,23 +7,36 @@ import {
     TouchableOpacity
 } from "react-native";
 
+import * as Animatable from "react-native-animatable";
+
+import { useNavigation } from "@react-navigation/native";
+
 export default function Welcome() {
+    const navigation = useNavigation();
+
     return (
         <View style={styles.container}>
             <View style={styles.containerLogo}>
-                <Image
+                <Animatable.Image
+                    animation="flipInY"
                     source={require('../../assets/logo.png')}
                     style={{ width: "100%" }}
                     resizeMode="contain"
                 />
             </View>
-            <View style={styles.containerForm}>
+            <Animatable.View
+                animation="fadeInUp"
+                delay={500}
+                style={styles.containerForm}>
                 <Text style={styles.title}>Organize e monitore seus gastos de qualquer lugar</Text>
                 <Text style={styles.text}>Faça o login para começar</Text>
-                <TouchableOpacity style={styles.button}>
+
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('SignIn')}
+                    style={styles.button}>
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
-            </View>
+            </Animatable.View>
         </View>
     )
 }
